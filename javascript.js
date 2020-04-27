ec09d066086b8ebed2c9e1a97d8d32ee9e281fe4
Mendofighter86 <61906372+Mendofighter86@users.noreply.github.com>
2020-04-27 15:11:36 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var impares = [];
+var pares = [];
+for (i=0; i<50; i++) {
+    var numero=Math.floor((Math.random()*100)+1);
+    if (numero % 2==0) {
+        pares.push(numero);
+    } else {
+        impares.push(numero);
+    }
+}
+alert (pares);
+alert (impares);
+
